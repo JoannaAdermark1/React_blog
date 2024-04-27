68a3743cd64b3d1a1edc7c6810609c1042f1ea39
JoannaAdermark1 Adermark <joanna.adermark@tutanota.com>
2024-04-27 11:26:06 +0000

    usingstage hook

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
+import {useState} from "react";
+
 const Home = () => {
+  // let name = 'mario';
+
+  const [name, setName ] = useState('mario');
+  const [age, setAge ] = useState (38);
+  const [job, setJob] = useState ('salesman');
+
+  const handleClick = () => {
+      setName('luigi');
+      setAge(32)
+      setJob('fullstack developer')
+  }
     return (
       <div className="home">
         <h2>Homepage</h2>
+        <p>{ name } is { age } years old and { job }</p>
+        <button onClick={handleClick}>click me </button>
       </div>
     );
   }
